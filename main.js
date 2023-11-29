f8350e22ad5a86e025086fe856edee9c96ccebfd
Riski032 <mr603470@gmail.com>
2023-11-29 20:27:34 +0700

    coba tambah

---
+++ main.js
@@ -0,0 +1,16 @@
+function ubah() {
+    document.getElementById("paragraf").style.color="red";
+    document.getElementById("paragraf").style.backgroundColor="red";
+    document.getElementById("paragraf").style.display="inline";
+  }
+  function ubah2() {
+    document.getElementById("paragraf").style.color="yellow";
+    document.getElementById("paragraf").style.backgroundColor="yellow";
+    document.getElementById("paragraf").style.display="inline";
+  }
+  function reset() {
+    document.getElementById("paragraf").style.color="";
+    document.getElementById("paragraf").style.backgroundColor="";
+    document.getElementById("paragraf").style.display="";
+  }
+  
